ba752f4ef9d11f130550c3b62ff9df3c22d40a82
alekbuza <github@aleksandarbuza.cr.rs>
2018-03-09 22:35:25 +0100

    Improved utils

--- libamazonproduct/utils.py
+++ libamazonproduct/utils.py
@@ -16,27 +16,41 @@
 
 import hmac
 import time
+import lxml
 import base64
 import hashlib
 
 from urllib.parse import quote
+from typing import Union
 
+from libamazonproduct.errors import InvalidAmazonCountryError
+
+
+# https://kdp.amazon.com/help?topicId=A1CT8LK6UW2FXJ
+AMAZON_DOMAINS = {
+    'CA': 'ca',
+    'DE': 'de',
+    'ES': 'es',
+    'FR': 'fr',
+    'IN': 'in',
+    'IT': 'it',
+    'JP': 'co.jp',
+    'UK': 'co.uk',
+    'US': 'com',
+    'CN': 'cn'
+}
 
 AMAZON_HTTP_METHOD = 'GET'
-AMAZON_URL = 'webservices.amazon.'
-AMAZON_URL_PATH = '/onca/xml'
+AMAZON_URL = 'webservices.amazon.{domain}'
+AMAZON_URL_WITH_ENDPOINT = 'webservices.amazon.{domain}/onca/xml'
+AMAZON_ENDPOINT= '/onca/xml'
+
+AMAZON_REQUEST_URL = 'http://{url}?{query}&Signature={signature}'
+AMAZON_QUERY_TO_SIGN = '{method}\n{url}\n{endpoint}\n{query}'
 
 
 def str_to_bytes(data: str) -> bytes:
     return bytes(data, 'utf-8')
-
-
-def url_with_cctld(cctld: str, path: bool = False) -> str:
-    if path:
-        return '%s%s%s' % (AMAZON_URL, cctld, AMAZON_URL_PATH)
-
-    else:
-        return '%s%s' % (AMAZON_URL, cctld)
 
 
 def bytes_to_quoted_base64(data: bytes) -> str:
@@ -56,15 +70,20 @@ def get_timestamp() -> str:
     return time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime())
 
 
-def query_to_sign(query: str, cctld: str) -> str:
-    return '%s\n%s\n%s\n%s' % (AMAZON_HTTP_METHOD,
-                               url_with_cctld(cctld=cctld),
-                               AMAZON_URL_PATH,
-                               query)
+def query_to_sign(query: str, country: str) -> str:
+    try:
+        return AMAZON_QUERY_TO_SIGN.format(method=AMAZON_HTTP_METHOD,
+                                           url=AMAZON_URL.format(
+                                               domain=AMAZON_DOMAINS[country]),
+                                           endpoint=AMAZON_ENDPOINT,
+                                           query=query)
+
+    except KeyError as e:
+        raise InvalidAmazonCountryError
 
 
-def hmac_sign(aws_secret_key: str, cctld: str, query: str) -> str:
-    message = query_to_sign(query=query, cctld=cctld)
+def hmac_sign(aws_secret_key: str, country: str, query: str) -> str:
+    message = query_to_sign(query=query, country=country)
     sign = hmac.new(key=str_to_bytes(aws_secret_key),
                     msg=str_to_bytes(message),
                     digestmod=hashlib.sha256).digest()
@@ -72,9 +91,37 @@ def hmac_sign(aws_secret_key: str, cctld: str, query: str) -> str:
     return sign
 
 
-def url_from_query_and_signature(query: str, signature: str, cctld: str) -> str:
-    url = 'http://%s?%s&Signature=%s' % (url_with_cctld(cctld=cctld, path=True),
-                                         query,
-                                         signature)
+def request_url(query: str, signature: str, country: str) -> str:
+    try:
+        url = AMAZON_REQUEST_URL.format(
+            url=AMAZON_URL_WITH_ENDPOINT.format(domain=AMAZON_DOMAINS[country]),
+            query=query,
+            signature=signature)
 
-    return url
+        return url
+
+    except KeyError as e:
+        raise InvalidAmazonCountryError
+
+
+class SafeXmlWrapper(object):
+    def __init__(self, xml: bytes):
+        self.__xml = xml
+
+    def _safe_get_xml_element(self, path, root=None):
+        elements = path.split('.')
+        parent = root if root is not None else self.__xml
+        for element in elements[:-1]:
+            parent = getattr(parent, element, None)
+            if parent is None:
+                return None
+
+        return getattr(parent, elements[-1], None)
+
+    def _safe_get_xml_element_text(self, path, root=None):
+        element = self._safe_get_xml_element(path, root)
+        if element is not None:
+            return element.text
+
+        else:
+            return 'None'
